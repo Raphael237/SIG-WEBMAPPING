30c77f74d730efa2abca69b4d128d5775786f851
= <=>
2024-12-03 10:03:56 +0100

    initialisation du projet

---
+++ src/main/java/com/elecam/ELECAM/ElecamApplication.java
@@ -0,0 +1,13 @@
+package com.elecam.ELECAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElecamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElecamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elecam/ELECAM/ElecamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elecam.ELECAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElecamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
